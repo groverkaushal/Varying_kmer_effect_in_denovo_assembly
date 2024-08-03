255b5c1e672f0f2da916a4a1d37c6f4563a46de2
Kaushal Grover <grover.kaushal27@gmail.com>
2024-08-03 23:11:57 +0530

    Update abyss_velvet_denovo_assembly.sh

--- abyss_velvet_denovo_assembly.sh
+++ abyss_velvet_denovo_assembly.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
 ### abyss assembly with varying kmer parameters
 mkdir abyss_outputs
 cd abyss_outputs
 
+kmer_start=39
+kmer_step=8
+kmer_end=152
+
 for kc in 2 3; do
-	for k in `seq 39 8 96`; do
+	for k in `seq $kmer_start $kmer_step $kmer_end`; do
 		mkdir k${k}-kc${kc}
 		abyss-pe -C k${k}-kc${kc} name=ecoli B=2G k=$k kc=$kc in='./../../2_HQ_filtered/output_1P  ./../../2_HQ_filtered/output_2P'
 		abyss-fac ./k${k}-kc${kc}/ecoli-unitigs.fa ./k${k}-kc${kc}/ecoli-contigs.fa ./k${k}-kc${kc}/ecoli-scaffolds.fa > ./k${k}-kc${kc}/abyss-fac_stat
@@ -22,7 +25,8 @@ cd ..
 mkdir velvet_outputs
 cd velvet_outputs
 
-for k in `seq 39 8 96`; do
+
+for k in `seq $kmer_start $kmer_step $kmer_end`; do
 	mkdir k${k}
 	./../Pre-requisite_tools/velvet-1.2.10/velveth ./k${k}/ ${k} -create_binary -shortPaired -fastq -separate ./../2_HQ_filtered/output_1P ./../2_HQ_filtered/output_2P
 	./../Pre-requisite_tools/velvet-1.2.10/velvetg ./k${k}/ -exp_cov auto
@@ -33,6 +37,3 @@ cd ..
 
 
 mkdir plots
-
-
-
